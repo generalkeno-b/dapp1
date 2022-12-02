ca56ea53742b34eee8c41832ad99f23f4560a84d
Aditya Kumar <f20200624@hyderabad.bits-pilani.ac.in>
2022-12-02 23:47:40 +0530

    almost complete

--- contracts/Rides.sol
+++ contracts/Rides.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 //import "https://github.com/ethereum/dapp-bin/blob/master/library/stringUtils.sol";
+//import "hardhat/console.sol";
 
 uint256 constant MAX_PASSENGERS = 4;
 
@@ -11,20 +12,32 @@ contract Rides {
   struct ridePass {
     uint256 price;
     address owner;
-    string fromAddress;
-    string toAddress;
-    uint distance;
-    
+    uint routeNo;
+    string[] route;
+    uint[] distance;
   }
 
   ridePass[MAX_PASSENGERS] public pass;
 
   constructor() {
     for (uint256 i = 0; i < MAX_PASSENGERS; i++) {
-      pass[i].toAddress = "Bangalore";
-      pass[i].fromAddress = "Hyderabad";
-      pass[i].distance = 250;
-      pass[i].price = 1e17*pass[i].distance; // 0.1 ETH per km
+      
+      //pass[i].price = 1e17*pass[i].distance; // 0.1 ETH per km
+      pass[i].routeNo = 5;
+      
+      pass[i].route.push("Thumkunta");
+      pass[i].distance.push(0);
+      pass[i].route.push("JBS");
+      pass[i].distance.push(15);
+      pass[i].route.push("Mehdipatnam");
+      pass[i].distance.push(25);
+      pass[i].route.push("Shamshabad");
+      pass[i].distance.push(35);
+      pass[i].route.push("Airport");
+      pass[i].distance.push(40);
+      
+      pass[i].price = 1e17;
+
       pass[i].owner = address(0x0);
     }
   }
@@ -42,9 +55,24 @@ contract Rides {
   function buyPass(uint256 _index, string memory _fromAddress, string memory _toAddress) external payable {
     require(_index < MAX_PASSENGERS && _index >= 0);
     require(pass[_index].owner == address(0x0));
-    require(msg.value >= pass[_index].price);
-    //require(_fromAddress == tickets[_index].fromAddress && _toAddress == tickets[_index].toAddress);
-    require(stringsEquals(_fromAddress, pass[_index].fromAddress) && stringsEquals(_toAddress, pass[_index].toAddress));
+
+    int l=-1;
+    int u=-1;
+    for(uint i=0;i<pass[_index].routeNo;i++)
+    {
+      if(stringsEquals(_fromAddress, pass[_index].route[i]))
+      {
+        l=int(i);
+      }
+      if(stringsEquals(_toAddress, pass[_index].route[i]))
+      {
+        u=int(i);
+      }
+    }
+    require(l!=-1 && u!=-1 && u>l);
+    // cost is (0.1 + 0.01*distance) ETH
+    require(msg.value >= ((pass[_index].price)+1e16*(pass[_index].distance[uint(u)] - pass[_index].distance[uint(l)])));
+
     pass[_index].owner = msg.sender;
   }
 }
--- test/Rides.js
+++ test/Rides.js
@@ -1,15 +1,68 @@
 const Rides = artifacts.require('Rides');
 const assert = require('assert');
-const _fromAddress = "Hyderabad", _toAddress = "Bangalore";
+
 contract('Rides', (accounts) => {
-    const passenger = accounts[1];
-    const ride_id = 0;
+    
     it('should a pasenger be able to get a ride pass', async () => {
+        const passenger = accounts[1];
+        const _fromAddress = "Thumkunta", _toAddress = "JBS";
+        const ride_id = 0;
         const instance = await Rides.deployed();
         const originalPass = await instance.pass(ride_id);
         await instance.buyPass(ride_id, _fromAddress, _toAddress, {
             from : passenger, 
-            value : originalPass.price,
+            value : (1e17+1e16*15),
+        });
+        const updatedPass = await instance.pass(ride_id);
+        assert.equal(
+            updatedPass.owner,
+            passenger,
+            'the passenger should now have his/her ride confirmed'
+        );
+    });
+    it('should a pasenger be able to get a ride pass', async () => {
+        const passenger = accounts[2];
+        const _fromAddress = "Thumkunta", _toAddress = "Airport";
+        const ride_id = 1;
+        const instance = await Rides.deployed();
+        const originalPass = await instance.pass(ride_id);
+        await instance.buyPass(ride_id, _fromAddress, _toAddress, {
+            from : passenger, 
+            value : (1e17+1e16*40),
+        });
+        const updatedPass = await instance.pass(ride_id);
+        assert.equal(
+            updatedPass.owner,
+            passenger,
+            'the passenger should now have his/her ride confirmed'
+        );
+    });
+    it('should a pasenger be able to get a ride pass', async () => {
+        const passenger = accounts[3];
+        const _fromAddress = "JBS", _toAddress = "Shamshabad";
+        const ride_id = 2;
+        const instance = await Rides.deployed();
+        const originalPass = await instance.pass(ride_id);
+        await instance.buyPass(ride_id, _fromAddress, _toAddress, {
+            from : passenger, 
+            value : (1e17+1e16*20),
+        });
+        const updatedPass = await instance.pass(ride_id);
+        assert.equal(
+            updatedPass.owner,
+            passenger,
+            'the passenger should now have his/her ride confirmed'
+        );
+    });
+    it('should a pasenger be able to get a ride pass', async () => {
+        const passenger = accounts[4];
+        const _fromAddress = "Mehdipatnam", _toAddress = "Airport";
+        const ride_id = 3;
+        const instance = await Rides.deployed();
+        const originalPass = await instance.pass(ride_id);
+        await instance.buyPass(ride_id, _fromAddress, _toAddress, {
+            from : passenger, 
+            value : (1e17+1e16*15),
         });
         const updatedPass = await instance.pass(ride_id);
         assert.equal(
